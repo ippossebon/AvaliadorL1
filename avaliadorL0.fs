790a87491c527cd339c074513fb6d75321ec65bc
victoriasimonetti <victoriasimonetti@gmail.com>
2016-10-11 20:13:43 -0300

    bugs arrumados

--- avaliadorL0.fs
+++ avaliadorL0.fs
@@ -2,7 +2,7 @@ type term =
     TTrue
     | TFalse
     | TIf of term * term * term
-    | TZero
+    | TZero 
     | TSucc of term
     | TPred of term
     | TIsZero of term
@@ -15,32 +15,48 @@ exception NoRuloApplies
 
 (* Funcao auxiliar: determinar se um termo eh um VALOR NUMERICO*)
 let rec is_numerical_value t = match t with
-    TZero -> true
+      TZero -> true
     | TSucc(t1) -> is_numerical_value t1
     | _ -> false
 
 (* Funcao STEP: -> avaliacao em um passo *)
 let rec step t = match t with
     (* Caso IF(t1, t2, t3)*)
-    TIf(TTrue, t2, t3) -> t2 (* IF TRUE *)
+      TIf(TTrue, t2, t3) -> t2 (* IF TRUE *)
     | TIf(TFalse, t2, t3) -> t3 (* IF FALSE *)
-    | TIf(t1, t2, t3) -> let t1' in TIf(t1', t2, t3)
+    | TIf(t1, t2, t3) -> let t1' = step t1 in TIf(t1' , t2, t3)
 
     | TSucc(t1) -> let t1' = step t1 in TSucc(t1')
 
     | TPred(TZero) -> TZero
-    | TPred(Tsucc(nv1)) when (is_numerical_value nv1) -> nv1
-    | TPred(nv1) -> let t1' = step t1 in TPred(t1')
+    | TPred(TSucc (nv1)) when (is_numerical_value nv1) -> nv1
+    | TPred(t1) -> let t1' = step t1 in TPred(t1')
 
     | TIsZero(TZero) -> TTrue
-    | TIsZero(TSucc(nv1)) when (is_numerical_value nv1) -> TFalse
+    | TIsZero(TSucc (nv1)) when (is_numerical_value nv1) -> TFalse
     | TIsZero(t1) -> let t1' = step t1 in TIsZero(t1')
-
     | _ -> raise NoRuloApplies
 
 
 (* Implementacao de EVAL *)
 let rec eval t =
+    printfn "estou no eval"
     try let t' = step t
         in eval t'
     with NoRuleApplies -> t
+
+
+[<EntryPoint>]
+    let main argv =
+        let t1 = TIsZero(TZero)
+        let t2 = TZero
+        let t3 = TSucc(TZero)
+        let t4 = TIsZero(TSucc(TZero))
+        let t5 = TIsZero(TFalse)
+
+        let resp = eval t4
+
+
+
+
+        0
