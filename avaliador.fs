91870d113b793bdbf9108d8a1d67b05133121be4
Isadora <isadorapossebon@gmail.com>
2016-11-21 21:39:22 -0200

    Nova versao apos tirar  duvidas

--- avaliador.fs
+++ avaliador.fs
@@ -1,3 +1,4 @@
+module avaliador
 (* Isadora Possebon - 00228551 Victória Portella - 00225886 *)
 (*
 Gramática:
@@ -58,21 +59,21 @@ type Expression =
     | Function of string * Type * Expression                        (* (fn string : T -> x + 1) e1  >>> Confirmar *)
     | Let of Expression * Expression * Expression                   (* let e1 = 5 *)
     | LetRec of string * Type * Type * Expression * Expression
-    | Nil (* Lista vazia *)
-    | Conc of Expression * Expression (* Concatenação de listas *)
+    | Nil                                                           (* Lista vazia *)
+    | Cons of Expression * Expression                               (* cons(e1, e2) Construção de listas *)
     | IsEmpty of Expression
-    | Hd of Expression (* Primeiro elemento da lista *)
-    | Tl of Expression (* Lista - primeiro elemento *)
+    | Hd of Expression                                              (* Primeiro elemento da lista: hd(1 2 4) -> 1 *)
+    | Tl of Expression                                              (* Lista - primeiro elemento tl(1 2 4) - > (2 4)*)
     | Raise
     | TryWith of Expression * Expression
 
-let rec isReady (e:Expression) : bool =   (* bool de F# *)
+let rec isValue (e:Expression) : bool =   (* bool de F# *) (* isValue *)
     match e with
-    Bool true -> true (* Valor Bool *)
-    | Bool false -> true (* Valor Bool *)
-    | Num e -> true (* Valor numérico *)
-    | Function(identifier, T1, e2) -> true (* fn x:T ⇒ e *)
+    Bool _ -> true (* Valor Bool *)
+    | Num _ -> true (* Valor numérico *)
+    | Function(_) -> true (* fn x:T ⇒ e *)
     | Nil -> true (* a confirmar *)
+    (*| Cons(e1, e2) -> Cons(, isValue e2) *)(* isValue e1 e isValue e2*)
     | e -> false (* Não está pronto *)
 
 
@@ -93,6 +94,16 @@ let rec replace (body:Expression) (var:Expression) (value:Expression) : Expressi
     | _ -> if body = var then value else body (* Let x: T => x  *)
 
 
+let rec replaceInLetRec (body:Expression) (var:string) (t1:Type) (t2:Type) (func:Expression) : Expression =
+    match body with
+    Applic(e1, e2) when (e1 = (Var var)) -> LetRec(var, t1, t2, func, Applic(Var var, e2))
+    | If(e1, e2, e3) -> If((replaceInLetRec e1 var t1 t2 func), (replaceInLetRec e2 var t1 t2 func), (replaceInLetRec e3 var t1 t2 func))
+    | BinOp(e1, op, e2) -> BinOp((replaceInLetRec e1 var t1 t2 func), op, (replaceInLetRec e2 var t1 t2 func))
+    | Applic(e1, e2) -> Applic((replaceInLetRec e1 var t1 t2 func), (replaceInLetRec e2 var t1 t2 func))
+    | Function(v, tp, f) -> Function(v, tp, (replaceInLetRec f var t1 t2 func))
+    | _ -> body
+
+
 (* Small Step *)
 let rec step (e:Expression) : Expression =
   match e with
@@ -101,60 +112,31 @@ let rec step (e:Expression) : Expression =
         | If(Bool false, e2, e3) -> e3 (* IF FALSE *)
         | If(e1, e2, e3) -> let e1' = step e1 in If(e1', e2, e3)
 
-        (* e1 e e2 estão prontos. *)
-        | BinOp (Num e1, Sum, Num e2) -> Num(e1+e2) (* Num(e1 + e2)*)
-        (* e2 não está pronto e precisa ser avaliado. *)
-        | BinOp (Num e1, Sum, e2) -> let e2' = step e2 in (BinOp(Num e1, Sum, e2'))
-        (* e1 não está pronto e precisa ser avaliado. *)
-        | BinOp (e1, Sum, e2) -> let e1' = step e1 in (BinOp(e1', Sum, e2))
+        | BinOp(Num n1, Operator, Num n2) ->
+            (match Operator with
+                Sum -> Num(n1+n2)
+                | Diff -> Num(n1-n2)
+                | Mult -> Num(n1*n2)
+                | Div -> Num(n1/n2)
+                | Equal -> Bool(n1 = n2)
+                | NotEqual -> Bool(n1 <> n2)
+                | Less -> Bool(n1 < n2)
+                | LessOrEqual -> Bool(n1 <= n2)
+                | Greater -> Bool(n1 > n2)
+                | GreaterOrEqual -> Bool(n1 >= n2))
+        | BinOp (Num e1, Operator, e2) -> let e2' = step e2 in (BinOp(Num e1, Operator, e2'))
+        | BinOp (e1, Operator, e2) -> let e1' = step e1 in (BinOp(e1', Operator, e2))
 
-        | BinOp (Num e1, Diff, Num e2) -> Num(e1-e2)
-        | BinOp (Num e1, Diff, e2) -> let e2' = step e2 in (BinOp(Num e1, Diff, e2'))
-        | BinOp (e1, Diff, e2) -> let e1' = step e1 in (BinOp(e1', Diff, e2))
-
-        | BinOp (Num e1, Mult, Num e2) -> Num(e1*e2)
-        | BinOp (Num e1, Mult, e2) -> let e2' = step e2 in (BinOp(Num e1, Mult, e2'))
-        | BinOp (e1, Mult, e2) -> let e1' = step e1 in (BinOp(e1', Mult, e2))
-
-        | BinOp (Num e1, Div, Num e2) -> Num(e1/e2) (* Devemos tratar se e2 = 0 *)
-        | BinOp (Num e1, Div, e2) -> let e2' = step e2 in (BinOp(Num e1, Div, e2'))
-        | BinOp (e1, Div, e2) -> let e1' = step e1 in (BinOp(e1', Div, e2))
-
-        | BinOp (Num e1, Equal, Num e2) -> Bool(e1=e2)
-        | BinOp (Num e1, Equal, e2) -> let e2' = step e2 in (BinOp(Num e1, Equal, e2'))
-        | BinOp (e1, Equal, e2) -> let e1' = step e1 in (BinOp(e1', Equal, e2))
-
-        | BinOp (Num e1, NotEqual, Num e2) -> Bool(e1<>e2)
-        | BinOp (Num e1, NotEqual, e2) -> let e2' = step e2 in (BinOp(Num e1, NotEqual, e2'))
-        | BinOp (e1, NotEqual, e2) -> let e1' = step e1 in (BinOp(e1', NotEqual, e2))
-
-        | BinOp (Num e1, Less, Num e2) -> Bool(e1<e2)
-        | BinOp (Num e1, Less, e2) -> let e2' = step e2 in (BinOp(Num e1, Less, e2'))
-        | BinOp (e1, Less, e2) -> let e1' = step e1 in (BinOp(e1', Less, e2))
-
-        | BinOp (Num e1, LessOrEqual, Num e2) -> Bool(e1<=e2)
-        | BinOp (Num e1, LessOrEqual, e2) -> let e2' = step e2 in (BinOp(Num e1, LessOrEqual, e2'))
-        | BinOp (e1, LessOrEqual, e2) -> let e1' = step e1 in (BinOp(e1', LessOrEqual, e2))
-
-        | BinOp (Num e1, Greater, Num e2) -> Bool(e1>e2)
-        | BinOp (Num e1, Greater, e2) -> let e2' = step e2 in (BinOp(Num e1, Greater, e2'))
-        | BinOp (e1, Greater, e2) -> let e1' = step e1 in (BinOp(e1', Greater, e2))
-
-        | BinOp (Num e1, GreaterOrEqual, Num e2) -> Bool(e1>=e2)
-        | BinOp (Num e1, GreaterOrEqual, e2) -> let e2' = step e2 in (BinOp(Num e1, GreaterOrEqual, e2'))
-        | BinOp (e1, GreaterOrEqual, e2) -> let e1' = step e1 in (BinOp(e1', GreaterOrEqual, e2))
-
-        | Applic(v, e2) when (isReady v) && not(isReady e2) -> let e2' = step e2 in Applic(v, e2')
-        | Applic(e1, e2) when not(isReady e1) -> let e1' = step e1 in Applic(e1', e2)
         | Applic(Function(identifier, tp, expression), value) -> replace expression (Var identifier) (value)
+        | Applic(v, e2) when (isValue v) && not(isValue e2) -> let e2' = step e2 in Applic(v, e2')
+        | Applic(e1, e2) when not(isValue e1) -> let e1' = step e1 in Applic(e1', e2)
 
-        (* let x = 2 in escopo *)
-        | Let(Var identifier, Num e1, e2) -> replace e2 (Var identifier) (Num e1)
+        (* let x = 2 in escopo --> Ainda nao funciona *)
+        | Let(Var identifier, Num n, e2) -> replace e2 (Var identifier) (Num n)
         | Let(Var identifier, Bool e1, e2) -> replace e2 (Var identifier) (Bool e1)
         | Let(Var identifier, e1, e2) -> let e1' = step e1 in Let(Var identifier, e1', e2)
 
-        | IsEmpty(Nil) -> true (* verificar *)
-        | IsEmpty(e1) -> false (* verificar*)
+        | LetRec(nm, t1, t2, f, e) -> replaceInLetRec(replace e (Var nm) (f)) (nm) (t1) (t2) (f)
 
         | _ -> raise NoRuleAppliesException (* termos prontos (incluindo Nil) retornam NoRuleAppliesException *)
 
@@ -171,4 +153,13 @@ let doisMaisCinco = eval(BinOp(Num 2, Sum, Num 5))
 let cincoMenosQuatro = eval(BinOp(Num 5, Diff, Num 4))
 let doisVezesTres = eval(BinOp(Num 2, Mult, Num 3))
 let seisDivDois = eval(BinOp(Num 6, Div, Num 2))*)
-let x = Applic(Function("x", Int, BinOp(Var "x", Sum, BinOp(Var "x", Mult, Num 2))), Num 6);
+(*let x = eval(Applic(Function("x", Int, BinOp(Var "x", Sum, BinOp(Var "x", Mult, Num 2))), Num 6)) *)(*x + 2x para x=6*)
+(*
+let funcaoDobro = eval(Applic(Function("x", Int, BinOp(Var "x", Mult, Num 2)), Num 30))
+let funcaoMax = If(BinOp(Var "in1", Greater, Var "in2"), Var "in1", Var "in2")
+let variavel = Let(Var "x", 5, ...)
+
+let testeLet = eval(Let(Var "oito", Num 8, Function("y", Int, BinOp(Var "y", Mult, Num 2))));;
+let testeLerRec = eval(LetRec("foo", Int, Int, Function("x", Int, (If ((BinOp(Var "x", GreaterOrEqual, Num 5)),(Var "x"),(Applic(Var "foo", BinOp(Var "x", Sum, Num 1)))))), Applic(Var "foo", Num 3)));;
+
+*)
